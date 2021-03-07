d3ed4bdd312b6cae07e3fa7b5df3dedbe2f7bc2c
ashleynwelker <ray13008@byui.edu>
2021-03-06 21:33:49 -0700

    Fix JS errors

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-var lastModified = new Date(document.lastModified);
-var currentYear = new Date();
-document.getElementById("last-modified").innerHTML = lastModified.toLocaleString();
-document.getElementById("current-year").innerHTML = currentYear.getFullYear();
+function dates() {
+    var lastModified = new Date(document.lastModified);
+    var currentYear = new Date();
+    document.getElementById("last-modified").innerHTML = lastModified.toLocaleString();
+    document.getElementById("current-year").innerHTML = currentYear.getFullYear();
+}
