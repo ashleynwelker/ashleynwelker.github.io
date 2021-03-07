5bfb50306a1cba55223a2ec800357c55492ee1a0
ashleynwelker <ray13008@byui.edu>
2021-03-06 21:09:47 -0700

    Add JS

---
+++ main.js
@@ -0,0 +1,4 @@
+var lastModified = new Date(document.lastModified);
+var currentYear = new Date();
+document.getElementById("last-modified").innerHTML = lastModified.toLocaleString();
+document.getElementById("current-year").innerHTML = currentYear.getFullYear();
