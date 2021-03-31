c85cbebefdd08663d6033c25a429e8f808a16033
ashleynwelker <ray13008@byui.edu>
2021-03-30 20:22:46 -0600

    Add Water Cycle and other minor changes

---
+++ lesson05/preston.js
@@ -0,0 +1,16 @@
+function dates() {
+    var lastModified = new Date(document.lastModified);
+    var currentYear = new Date();
+    document.getElementById("last-modified").innerHTML = lastModified.toLocaleString();
+    document.getElementById("current-year").innerHTML = currentYear.getFullYear();
+}
+
+function toggleMenu() {
+    var list = document.getElementById("menu");
+    if (list.style.display === "block") {
+        list.style.display = "none";
+    } 
+    else {
+        list.style.display = "block";
+    }
+}
